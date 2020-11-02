3c55017e1decc58d50c4520cda1bc8f1498bf4a9
mwang20 <mwang20@stuy.edu>
2020-11-02 01:47:59 -0500

    I realized I hadn't put anything under the Triangle constructors, so I did, and I tested my getPerimeter function with select coordinates which printed out approxiamately 17 as expected.

--- Tester.java
+++ Tester.java
@@ -2,6 +2,7 @@ public class Tester{
   public static void main(String[] args){
     Point testPoint = new Point(1, 2);
     Point other = new Point(1, 6);
+    Triangle testTriangle = new Triangle(0, 0, 0, 5, 5, 5);
     System.out.println(testPoint.getX()); //Should return 1
     System.out.println();
     System.out.println(testPoint.getY()); //Should return 2
@@ -11,5 +12,7 @@ public class Tester{
     System.out.println(testPoint.equals(other)); //Should return false
     System.out.println();
     System.out.println(testPoint.equals(testPoint)); //Should return true
+    System.out.println();
+    System.out.println(testTriangle.getPerimeter()); //Should return approxiamately 17
   }
 }
