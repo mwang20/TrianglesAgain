7bce2746cebd1e38104bdb1afc9e893e4fae8966
mwang20 <mwang20@stuy.edu>
2020-11-02 00:06:22 -0500

    Put Point, getX, and getY and tested them. Returned expected coordinates. Works fine.

--- Point.java
+++ Point.java
@@ -1,3 +1,13 @@
 public class Point{
   private double x, y;
+  public Point(double X, double Y){
+    x = X;
+    y = Y;
+  }
+  public double getX(){
+    return x;
+  }
+  public double getY(){
+    return y;
+  }
 }
--- Tester.java
+++ Tester.java
@@ -1,4 +1,9 @@
 public class Tester{
   public static void main(String[] args){
+    Point testPoint = new Point(1, 2);
+    System.out.println(testPoint.getX()); //Should return 1
+    System.out.println();
+    System.out.println(testPoint.getY()); //Should return 2
+    System.out.println();
   }
 }
